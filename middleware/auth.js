48cdbabc3c0275856b24f5297c6cf4e127f9bd93
Ayushman <ayushman.gigs@gmail.com>
2026-07-23 20:26:01 +0530

    API auth error fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,7 +10,7 @@ function auth(req, res, next) {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.userId = decoded.id;
+        req.userId = decoded.userId;
         next();
     } catch (err) {
         if (err instanceof jwt.TokenExpiredError || err instanceof jwt.JsonWebTokenError) {
